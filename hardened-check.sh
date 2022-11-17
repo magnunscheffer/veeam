ba16851ea28db3b1b32559a0047ac7d65bdf5f3c
magnunscheffer <mfs_@outlook.com>
2022-11-17 18:11:58 -0300

    Update hardened-check.sh
    Adding descriptions

--- hardened-check.sh
+++ hardened-check.sh
@@ -17,12 +17,14 @@ echoI() {
 } 
 terms="N"
 echoE "
-----------------------------------------------------------------------------------------------------------------------------------------------
-| This script is independently produced and has no direct link to Veeam Software. It just checks the recommendations related to the article: |
-| https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository.html?ver=110                                                          |
-|                                                                                                                                            |
-| By using this script you are at your own risk. Do you accept these terms? [Y=YES or N=NO]:                                                 |
-----------------------------------------------------------------------------------------------------------------------------------------------"
+-----------------------------------------------------------------------------------------------------------------------------------------------
+| This script is independently produced and has no direct link to Veeam Software. It just checks the recommendations related to the user guide|
+| article and Veeam Write Paper "Protect against Ransomware with Immutable Backups":                                                          |
+| - https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository.html?ver=110                                                         |
+| - https://www.veeam.com/wp-guide-protect-ransomware-immutable-backups.html                                                                  |
+|                                                                                                                                             |
+| By using this script you are at your own risk. Do you accept these terms? [Y=YES or N=NO]:                                                  |
+-----------------------------------------------------------------------------------------------------------------------------------------------"
 read terms
 
 if [ -z "$terms" ] || [ ${terms^^} != "Y" ] 
@@ -61,9 +63,19 @@ dirowner=$(ls -ld $repo | awk '{print $3}')
 
 if [ "$dirowner" == "$svcuser" ]
 then 
-    echoI "Info: The service user '$svcuser' and the dir owner '$dirowner' are set correctly!"
+    echoI "
+    Info: The service user '$svcuser' and the dir owner '$dirowner' are set correctly!
+
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-1--prepare-directory-on-linux-server-for-backups
+    Reference: 'Both owner and group can be the account that you plan to use to connect to the Linux server.'
+    "
 else 
-    echoW "Warning: The user: $svcuser is different from the folder owner: $dirowner !"
+    echoW "Warning: The user: $svcuser is different from the folder owner: $dirowner !, Please connect the repository to veeam using single-use credentials account $dirowner.
+    
+    More details:
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-1--prepare-directory-on-linux-server-for-backups
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-2--add-linux-server-to-backup-infrastructure
+    Reference: 'Use temporary credentials to avoid storing the credentials in the Veeam Backup & Replication configuration database. To do that, click Add and select Single-use credentials for hardened repository'"
 fi
 
 echoD
@@ -73,9 +85,15 @@ permission=$(ls -ld $repo | awk '{print $1}')
 
 if [ "$permission" == "$permok" ]
 then 
-    echoI "Info: The repository '$repo' is configured with the correct permission '$permission'(700)"
+    echoI "Info: The repository '$repo' is configured with the correct permission '$permission'(700)
+
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-1--prepare-directory-on-linux-server-for-backups
+    Reference: 'To allow access to the folder only for its owner and root account: chmod 700 <folder_path>'"
 else 
-    echoW "Warning: Permission of repository '$repo' is wrong! ($permission), please set it to '$permok'(Ex: 'chmod 700 $repo')!"
+    echoW "Warning: Permission of repository '$repo' is wrong! ($permission), please set it to '$permok'(Ex: 'chmod 700 $repo')!
+
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-1--prepare-directory-on-linux-server-for-backups
+    Reference: 'To allow access to the folder only for its owner and root account: chmod 700 <folder_path>'"
 fi
 
 echoD 
@@ -86,9 +104,16 @@ sudoresult=$(sudo -l -U $svcuser)
 
 if [ "$sudoresult" == "$messagenosudo" ]
 then 
-    echoI "Info: The user $svcuser doesn't have SUDO access, this is a good practice!"
+    echoI "Info: The user $svcuser doesn't have SUDO access, this is a good practice!
+
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-2--add-linux-server-to-backup-infrastructure
+    Reference: 'After the user will have temporary root- or sudo-permissions you must remove the user from the sudo group after the server is added.'"
+
 else 
-    echoW "Warning: Possible security breach!, user: $svcuser has SUDO rights!!!!"
+    echoW "Warning: Possible security breach!, user: $svcuser has SUDO rights, please disable the SUDO!
+      
+    https://helpcenter.veeam.com/docs/backup/vsphere/hardened_repository_deploy.html?ver=110#step-2--add-linux-server-to-backup-infrastructure
+    Reference: 'After the user will have temporary root- or sudo-permissions you must remove the user from the sudo group after the server is added.'"
 fi
 
 echoD
@@ -101,11 +126,18 @@ do
    : 
    if ([ $i -lt 2500 ] || [ $i -gt 3300 ] && [ $i != "6162" ] )
    then
-        echoW "Warning: $i is not a veeam service port, please disable it!"	
+        echoW "Warning: $i is not a veeam service port, please disable it!"
    else 
-	echoI "Info: $i is a veeam service/transport port."
+	echoI "Info: $i is a veeam service/transport port."    
    fi
 done
+
+echoD 	""
+echoD 	"https://www.veeam.com/wp-guide-protect-ransomware-immutable-backups.html                                                    
+        Reference: (Page 17): 
+        '8. Only run the Veeam transport service available on the network (SSH can be an exception).           
+        There should especially be no third-party network services running with root permissions.                                   
+        If an attacker can gain root access via a third-party software on the Hardened Repository, then they can delete all data.'"
 echoD 	"--------------------------------------------------------------------------------"
 echoD
 

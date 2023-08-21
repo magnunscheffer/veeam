d6fa3ded6a7e881abdf4f9c8a63bb446497e8e7a
magnunscheffer <mfs_@outlook.com>
2023-08-21 19:20:56 -0300

    Fix Comments

--- selfportalem-forcetemplate/SelfJobEntMgr-ForceTemplateValues.ps1
+++ selfportalem-forcetemplate/SelfJobEntMgr-ForceTemplateValues.ps1
@@ -3,11 +3,13 @@
 - Visit https://github.com/magnunscheffer/veeam/tree/main/selfjob-entmgr#requirements-for-this-script
 
 .DESCRIPTION
- ]This script change EM Self-Service Portal Tenant Jobs settings to force use values from a Template Job[Overriding the user settings], for more details about EM Self-Service Portal visit:
- https://helpcenter.veeam.com/docs/backup/em/em_working_with_vsphere_portal.html?ver=120
+ This script will change the EM Self Service Portal tenant jobs  to force the use of predefined options/scheduling from a VBR template job [overriding user settings]. 
+ For more details about EM Self-Service Portal visit: https://helpcenter.veeam.com/docs/backup/em/em_working_with_vsphere_portal.html?ver=120
 .EXAMPLE 
  Put this script on "Pre-Script" session on your Template Job and after that, use 'Copy from" Settings during the Tenant Creation. For more information access:
  https://helpcenter.veeam.com/docs/backup/em/em_adding_tenant_accounts.html?ver=120  (Step 15-c)
+
+ Full guidance on: https://github.com/magnunscheffer/veeam/tree/main/selfportalem-forcetemplate
 
 
 .NOTES
@@ -37,16 +39,17 @@ $TemplateJob = Get-VBRJob -Name $TemplateName
 $TenantJob = Get-VBRJob -Name $jobname
 #$TenantJob = Get-VBRJob -Name "TenantMagnun"  #use for internal debug only
 
-#Reading Options and Schedule from Template Job.
+#Reading Options and Schedule of Template Job.
 $Template0ptions = Get-VBRJobOptions $TemplateJob
 $TemplateSchedule = Get-VBRJobScheduleOptions -Job $TemplateJob
 
-#Getting Current Tenanat Settings
+#Getting Current Tenant Options and Schedule
 $Tenant0ptions = Get-VBRJobOptions $TenantJob
 $TenantSchedule = Get-VBRJobScheduleOptions -Job $TenantJob
 
-#Preserve specifics parameters from Tenant Job
-$TemplateSchedule.OptionsBackupWindow = $TenantSchedule.OptionsBackupWindow  #On this case is force to keep the user defined backup windows instead replace with Template job settings.
+#Preserve tenant job-specific parameters
+#In this example the Job will preserve the backup window defined by the user.
+$TemplateSchedule.OptionsBackupWindow = $TenantSchedule.OptionsBackupWindow  
 
 #Setting Template values to Tenant Job
 Set-VBRJobOptions -Job $TenantJob -Options $Template0ptions
